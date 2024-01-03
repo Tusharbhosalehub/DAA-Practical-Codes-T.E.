4d0f8a608c60a18b8437d89b54f74ad9fcac68a2
Tushar Bhosale <114877970+Tusharbhosalehub@users.noreply.github.com>
2024-01-04 02:06:51 +0530

    Update TSP_using_branch&bound.c

--- TSP_using_branch&bound.c
+++ TSP_using_branch&bound.c
@@ -11,3 +11,13 @@ int distanceMatrix[N][N] = {
     {15, 35, 0, 30},
     {20, 25, 30, 0}
 };
+void tspBranchAndBound(int level, int cost, int path[N], int bestPath[N], int& minCost) {
+    if (level == N) {
+        cost += distanceMatrix[path[level - 1]][0];
+        if (cost < minCost) {
+            minCost = cost;
+            for (int i = 0; i < N; i++) {
+                bestPath[i] = path[i];
+            }
+        }
+        return;

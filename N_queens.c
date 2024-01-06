624a259733d0638c9a934b1cf8a8418ac2c300c7
Tushar Bhosale <114877970+Tusharbhosalehub@users.noreply.github.com>
2024-01-06 21:38:19 +0530

    Update N_queens.c

--- N_queens.c
+++ N_queens.c
@@ -9,3 +9,38 @@ bool isSafe(vector<int>& board, int row, int col) {
         }
     }
     return true;
+}
+
+void printBoard(vector<int>& board) {
+    int n = board.size();
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (board[i] == j) {
+                cout << "Q" << i + 1 << " ";
+            } else {
+                cout << "- ";
+            }
+        }
+        cout << endl;
+    }
+}
+
+bool solveNQueensUtil(vector<int>& board, int row) {
+    int n = board.size();
+    if (row == n) {
+        printBoard(board);
+        cout << endl;
+        return true;
+    }
+
+    bool res = false;
+    for (int i = 0; i < n; i++) {
+        if (isSafe(board, row, i)) {
+            board[row] = i;
+            res = solveNQueensUtil(board, row + 1) || res;
+            board[row] = -1; // Backtrack
+        }
+    }
+
+    return res;
+}
